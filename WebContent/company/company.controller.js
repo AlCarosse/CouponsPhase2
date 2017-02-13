8d0e57e00b3bd10de02b0c62f10c5503d3057496
Raviv Shweid <raviv.shweid@telia.no>
2017-02-13 23:28:04 +0200

    Add company view

---
+++ WebContent/company/company.controller.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CompanyController', CompanyController);
+
+    CompanyController.$inject = ['UserService', '$rootScope', 'AuthenticationService'];
+    
+    function CompanyController(UserService, $rootScope, AuthenticationService) {
+        var vm = this;
+        
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+
+        initController();
+
+        function initController() {
+        	loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUserOld() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadCurrentUser() {
+        	vm.user = $rootScope.globals.currentUser;
+        }
+
+
+        
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+    }
+
+})();
