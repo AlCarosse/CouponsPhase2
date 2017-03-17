983b5b4f8e6323ca6ac238f7c6cbe4addf0369bf
Raviv Shweid <raviv.shweid@telia.no>
2017-03-17 18:43:16 +0200

    Clean customer.home.controller.js

--- WebContent/customer/customer.home.controller.js
+++ WebContent/customer/customer.home.controller.js
@@ -11,42 +11,16 @@
         var vm = this;
         
         vm.user = null;
-        vm.allUsers = [];
-        vm.deleteUser = deleteUser;
 
         initController();
 
         function initController() {
         	loadCurrentUser();
-            loadAllUsers();
-        }
-
-        function loadCurrentUserOld() {
-        	UsersService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
-                    vm.user = user;
-                });
         }
 
         function loadCurrentUser() {
         	vm.user = $rootScope.globals.currentUser;
         }
 
-
-        
-        function loadAllUsers() {
-        	UsersService.GetAll()
-                .then(function (users) {
-                    vm.allUsers = users;
-                });
-        }
-
-        function deleteUser(id) {
-        	UsersService.Delete(id)
-            .then(function () {
-                loadAllUsers();
-            });
-        }
     }
-
 })();
