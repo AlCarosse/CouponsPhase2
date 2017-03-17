16defa9fa2403bf5ad2c8669f73db4ffcb98bbca
Raviv Shweid <raviv.shweid@telia.no>
2017-03-18 00:27:06 +0200

    Clean company.coupons.controller.js

--- WebContent/company/company.coupons.controller.js
+++ WebContent/company/company.coupons.controller.js
@@ -11,18 +11,12 @@
         
     	var vm = this;
         vm.user = null;
-        
-        vm.currentCompany = null;
-        vm.currentCompanyNew = null;
-        vm.updateCompanyStatus = null;
-        
-        vm.coupons = [];
-        
+                
+        vm.coupons = [];        
         vm.currentCoupon = null;
         vm.currentCouponNew = null;
         vm.updateCouponStatus = null;
 
-        
         vm.getCompanyCouponsQuery	= getCompanyCouponsQuery;
         vm.getCompanyCoupons 		= getCompanyCoupons;
         vm.deleteCoupon 			= deleteCoupon;
@@ -30,28 +24,12 @@
         
         vm.setCurrentCoupon = setCurrentCoupon;
         vm.clearFilter	    = clearFilter;
-
-        
-		vm.newCoupon = {
-			  	 	"couponId"       :  0
-			    	,"couponEndDate" : ""
-			    	,"couponPrice"   : 0
-			 };
-
-		vm.filter = {
-			  	 	"couponTypeId"  :  null
-			  	    ,"fromPrice" 	 :  null
-			  	   	,"toPrice" 	     :  null
-			  	   	,"fromDate" 	 :  null
-			  	   	,"toDate" 	     :  null  
-			  	};
-        
 		
         initController();
 
         function initController() {
         	loadCurrentUser();
-        	getCompanyCoupons();
+        	clearFilter();
         }
 
         function clearFilter() {
@@ -64,7 +42,6 @@
 			  	};
         	getCompanyCouponsQuery();
         }
-
 
         function getCompanyCouponsQuery() 
         {		
@@ -102,34 +79,41 @@
         }
         
         function updateCoupon() 
-        {		
-        	vm.newCoupon.couponId = vm.currentCouponNew.couponId;
-        	vm.newCoupon.couponPrice = vm.currentCouponNew.couponPrice;
+        {
+    		vm.couponForUpdate = {
+			  	 	"couponId"       :  0
+			    	,"couponEndDate" : ""
+			    	,"couponPrice"   : 0
+			 };
 
-			var yyyymmdd = vm.currentCouponNew.couponEndDate.toISOString().slice(0,10).replace(/-/g,"");
-			vm.newCoupon.couponEndDate = yyyymmdd;
+        	vm.couponForUpdate.couponId = vm.currentCouponNew.couponId;
+        	vm.couponForUpdate.couponPrice = vm.currentCouponNew.couponPrice;
+
+    		var yyyymmdd = moment(vm.currentCouponNew.couponEndDate).format('YYYYMMDD');
+			vm.couponForUpdate.couponEndDate = yyyymmdd;
         	
-			CouponsService.UpdateCoupon(vm.newCoupon,	function (response) 
+			CouponsService.UpdateCoupon(vm.couponForUpdate,	function (response) 
             {
                 if (response.data.serviceStatus.success === "true") 
                 {
                 	vm.currentCoupon.couponPrice   = vm.currentCouponNew.couponPrice;
                 	vm.currentCoupon.couponEndDate = vm.currentCouponNew.couponEndDate;
                 	vm.updateCouponStatus = "success";
+                	getCompanyCouponsQuery();
                 } 
                 else 
                 {
                 	vm.updateCouponStatus = "fail";
                 	vm.errorMesage = response.data.serviceStatus.errorMessage;                    
                 }
-            });            
+            });
         }
 
         function setCurrentCoupon(coupon) {
 			vm.currentCoupon = coupon;       	
 			vm.currentCouponNew = angular.copy(coupon);
 			
-			var price = Math.floor (vm.currentCouponNew.couponPrice); 
+			var price = Math.floor ( vm.currentCouponNew.couponPrice ); 
 			vm.currentCouponNew.couponPrice = price;
 				        	
 			var st = vm.currentCouponNew.couponEndDateYyyyMmDd;
@@ -140,11 +124,9 @@
 			
 			vm.updateCouponStatus = null;
         }
-
      
         function deleteCoupon( couponId , couponTitle ) 
         {
-        	
         	bootbox.confirm
         	({
         		message: "Remove " + couponTitle + "?",
@@ -171,21 +153,14 @@
                             vm.dataLoading = false;
                         }
                     });
-                  
-                    
-                    
-                    
                  }
-        	});
-        	        	
+        	});        	
         };
-
         
-        
-        function getCompanyCoupons() {
+        function getCompanyCoupons() 
+        {
             vm.dataLoading = true;
-            CouponsService.GetCompanyCoupons(	 
-            							function (response) 
+            CouponsService.GetCompanyCoupons( function (response) 
             {
                 if (response.data.serviceStatus.success === "true") 
                 {
@@ -202,14 +177,11 @@
                 }
             });
         };
-
-        
         
         function loadCurrentUser() {
         	vm.user = $rootScope.globals.currentUser;
         }
 
               
-        
     }
 })();
